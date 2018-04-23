ce0e889235a8d66772b994d22a4d4f609c38198a
Brandon Azad <bazad@cs.stanford.edu>
2018-04-23 12:12:26 -0700

    Initial Xcode static library project

---
+++ ios-command-line-tool/ios_command_line_tool.h
@@ -0,0 +1,13 @@
+//
+//  ios_command_line_tool.h
+//  ios-command-line-tool
+//
+//  Created by Brandon Azad on 4/23/18.
+//  Copyright © 2018 Brandon Azad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_command_line_tool : NSObject
+
+@end
---
+++ ios-command-line-tool/ios_command_line_tool.m
@@ -0,0 +1,13 @@
+//
+//  ios_command_line_tool.m
+//  ios-command-line-tool
+//
+//  Created by Brandon Azad on 4/23/18.
+//  Copyright © 2018 Brandon Azad. All rights reserved.
+//
+
+#import "ios_command_line_tool.h"
+
+@implementation ios_command_line_tool
+
+@end
